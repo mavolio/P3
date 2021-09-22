ec6c4b65cc73ff103d412f93b5b10593f8cf525e
Meghan Avolio <mavolio2@jh.edu>
2021-09-22 17:32:47 -0400

    using P3 data for this and fixing figures

--- Community_composition.R
+++ Community_composition.R
@@ -5,15 +5,17 @@ library(tidyverse)
 library(ggrepel)
 library(gridExtra)
 
-theme_set(theme_bw(20))
+theme_set(theme_bw(12))
 
 #set wd
 my.wd<-setwd("C:/Users/mavolio2/Dropbox/Konza Research")
 
 #read in data
-pplotcomp<-read.csv(paste(my.wd, "/pplots/Sppcomp/Species Comp_to Use/Compiling Datasets in R/Spp_Data_2002_2019.csv", sep = ""))%>%
-  filter(calendar_year>2009&calendar_year<2016)%>%
-  filter(treatment=="N1P0"|treatment=="N2P0"|treatment=="N1P3"|treatment=="N2P3")
+treats<-read.csv(paste(my.wd, "/p-cubed/Analyses/July 2015 Analyses/PPlot_PlotList.csv", sep=""))
+
+pplotcomp<-read.csv(paste(my.wd, "/p-cubed/SpComp/pcubed_sp_data2010-2015.csv", sep = ""))%>%
+  filter(precip=="control")%>%
+  left_join(treats)
 
 cattraits<-read.csv(paste(my.wd, "/pplots/traits_2021.csv", sep=""))%>%
   filter(Genus!="")%>%
@@ -27,8 +29,8 @@ cattraits<-read.csv(paste(my.wd, "/pplots/traits_2021.csv", sep=""))%>%
 
 #get average cover of each species in a treatment for each year of the experiment (average over the plots)
 ave<-pplotcomp%>%
-  group_by(calendar_year, treatment, genus_species)%>%
-  summarize(mabund=mean(abundance))
+  group_by(year, Trt, genus_species)%>%
+  summarize(mabund=mean(cover))
 
 #make the dataset wide for vegan
 compwide<-ave%>%
@@ -38,38 +40,38 @@ compwide<-ave%>%
 plots<-compwide[,1:2]
 
 #run nmds
-mds<-metaMDS(compwide[,3:69])
+mds<-metaMDS(compwide[,3:73])
 mds
 
 #extract NMDS coordinates and bind to plot info for graphs
 #the label step adds the label for the first and last year of data
 scores<-plots%>%
   bind_cols(as.data.frame(mds$points))%>%
-  mutate(label=ifelse(calendar_year==2010|calendar_year==2015, as.character(calendar_year),""))
+  mutate(label=ifelse(year==2010|year==2015, as.character(year),""))
 
 #make figure with first and last year labeled and path between points connected
 NMDS<-
-ggplot(data=scores, aes(x=MDS1, y=MDS2, color=treatment, label=label))+
+ggplot(data=scores, aes(x=MDS1, y=MDS2, color=Trt, label=label))+
   geom_point(size=3)+
   geom_path()+
   geom_text_repel(show.legend = F)+
-  scale_color_manual(name="Treatment", values = c("black", "blue", "red", "purple"), labels=c("Control", "P", "N", "N+P"))+
+  scale_color_manual(name="Nutrient treatment", values = c("black", "blue", "red", "purple"), breaks=c("Control", "P", "N", "P&N"), labels=c("Control", "P", "N", "N+P"))+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
   ylab("NMDS2")+
   xlab("NMDS1")
 
 #permanova
-adonis(compwide[3:69]~compwide$treatment)
+adonis(compwide[3:73]~compwide$Trt)
 
 #perm dispersion
-dist<-vegdist(compwide[3:69])
-betadisp<-betadisper(dist,compwide$treatment,type="centroid")
+dist<-vegdist(compwide[3:73])
+betadisp<-betadisper(dist,compwide$Trt,type="centroid")
 permutest(betadisp)
 
 #get average cover of each species in each treatment over all years (average over plots and years). 
 #join categorical triats and create trait categories
 racave<-ave%>%
-  group_by(treatment, genus_species)%>%
+  group_by(Trt, genus_species)%>%
   summarize(mabund=mean(mabund))%>%
   mutate(rank=rank(-mabund, ties.method = "first"))%>%
   left_join(cattraits)%>%
@@ -82,29 +84,26 @@ racave<-ave%>%
   separate(genus_species, into=c("genus", "species"), sep="_")%>%
   mutate(genera=toupper(substr(genus, 1, 1)),
          sp=paste(genera, species, sep=". "))%>%
-  mutate(name=ifelse(rank<6, sp, ""))
+  mutate(name=ifelse(rank<6, sp, ""))%>%
+  mutate(trt2=factor(Trt, levels=c("Control", "P", "N", "P&N")))
 
 #make new labels for facet_wrap step  
-collabel<-c(
-  N1P0="Control", 
-  N1P3="P",
-  N2P0="N",
-  N2P3="N+P")
+collabel<-c("Control"="Control", "P"="P","N"="N","P&N"="N+P")
 
 #great rac figure
 rac<-
 ggplot(data=racave, aes(x=rank, y=mabund, label=name))+
   geom_line()+
-  geom_point(aes(color=trait_cat), size=4)+
-  scale_color_manual(name="Functional Type", values=c("darkgreen", "chartreuse3", "green", "darkblue", "lightblue", "deepskyblue"), breaks = c("C4 Gram.", "C3 Gram.",  "Annual Gram.","Non-N-Fixing Forb", "N-Fixing Forb", "Annual Forb"))+
-  #geom_text_repel(max.overlaps = 10, size=5)+
-  facet_wrap(~treatment, labeller = labeller(treatment=collabel))+
+  geom_point(aes(color=trait_cat), size=2)+
+  scale_color_manual(name="Functional type", values=c("darkgreen", "chartreuse3", "green", "darkblue", "lightblue", "deepskyblue"), breaks = c("C4 Gram.", "C3 Gram.",  "Annual Gram.","Non-N-Fixing Forb", "N-Fixing Forb", "Annual Forb"))+
+  geom_text_repel(max.overlaps = 10, size=3)+
+  facet_wrap(~trt2, labeller = labeller(trt2=collabel))+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
   ylab("Abundance")+
   xlab("Rank")
 
 #bind both figures together.
-grid.arrange(NMDS, rac, ncol=2)
+grid.arrange(NMDS, rac)
 
 #plot changes over time ESA talk
 
