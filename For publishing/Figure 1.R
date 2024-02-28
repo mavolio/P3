5650d95eec9debbc865025f1fff39d7b90cdedba
mavolio <meghan.avolio@jhu.edu>
2024-02-28 11:48:45 -0500

    making figures for figure 1

--- For publishing/Figure 1.R
+++ For publishing/Figure 1.R
@@ -65,7 +65,8 @@ ggplot(data=scores, aes(x=MDS1, y=MDS2, color=Trt, fill=label, shape=Drought, gr
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
   ylab("NMDS2")+
   xlab("NMDS1")+
-  annotate("text", x = 0.9, y = -.5, label="Stress = 0.18")
+  annotate("text", x = 0.9, y = -.5, label="Stress = 0.18")+
+  ggtitle("A) Community compopostion over the\n14-year nutrient addition  experiment")
 NMDS
 
 #permanova
@@ -82,6 +83,7 @@ permutest(betadisp)
 PFTcover<-pplotcomp%>%
   left_join(cattraits) %>% 
   filter(spnum<900) %>% 
+  filter(calendar_year==2010) %>% 
   mutate(trait_cat=ifelse(growthform=="F"&lifespan=="A", "Annual Forb",
                    ifelse(growthform=="G"&lifespan=="A", "Annual Grass",
                    ifelse(growthform=="G"&photopath=="C3", "Peren C3 Grass",
@@ -111,7 +113,7 @@ ggplot(data=racave, aes(x=rank, y=meancover))+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
   ylab("Cover")+
   xlab("Rank")+
-  ggtitle("Cover of Plant Functional Types")
+  ggtitle("B) Cover of plant functional types in 2010,\nthe first year of the drought treatments")
 rac
 
 #bind both figures together.
@@ -121,56 +123,3 @@ grid.arrange(NMDS, rac)
 
 grid.arrange(NMDS, rac_func)
 
-
-
-#plot changes over time ESA talk
-
-# allpp<-read.csv(paste(my.wd, "/pplots/Sppcomp/Species Comp_to Use/Compiling Datasets in R/Spp_Data_2002_2019.csv", sep = ""))%>%
-#   filter(treatment=="N1P0"|treatment=="N2P0"|treatment=="N1P3"|treatment=="N2P3")
-# 
-# cattraits<-read.csv(paste(my.wd, "/pplots/traits_2021.csv", sep=""))%>%
-#   filter(Genus!="")%>%
-#   select(Genus, Species, Annual.Peren.Bi, Forb.grass.shrub, C3.C4, N.fixer..Y.N...)%>%
-#   mutate(genus_species=paste(tolower(Genus), Species, sep="_"))%>%
-#   rename(lifespan=Annual.Peren.Bi,
-#          growthform=Forb.grass.shrub,
-#          photopath=C3.C4,
-#          Nfix=N.fixer..Y.N...)%>%
-#   select(-Genus, -Species)
-
-#get average cover of each species in a treatment for each year of the experiment (average over the plots)
-aveall<-allpp%>%
-  group_by(calendar_year, treatment, genus_species)%>%
-  summarize(mabund=mean(abundance))
-
-
-                     
-#get average cover of each species in each treatment over all years (average over plots and years). 
-#join categorical triats and create trait categories
-ractime<-aveall%>%
-  left_join(cattraits)%>%
-  mutate(trait_cat=ifelse(growthform=="F"&lifespan=="A", "Annual Forb",
-                          ifelse(growthform=="G"&lifespan=="A", "Annual Grass",
-                                 ifelse(growthform=="G"&photopath=="C3", "C3 Gram.",
-                                        ifelse(growthform=="G"&photopath=="C4", "C4 Grass",
-                                               ifelse(growthform=='F'|growthform=="S"&Nfix=="N", "Non-N-Fixing Forb",
-                                                      ifelse(growthform=='F'|growthform=="S"&Nfix=="Y", "N-Fixing Forb","UNK")))))))%>%
-  group_by(trait_cat, calendar_year, treatment)%>%
-  summarise(abund=sum(mabund))
-
-#make new labels for facet_wrap step  
-collabel<-c(
-  N1P0="Control", 
-  N1P3="P",
-  N2P0="N",
-  N2P3="N+P")
-
-ggplot(data=ractime, aes(x=calendar_year, y=abund, group=trait_cat))+
-  geom_line()+
-  geom_point(aes(color=trait_cat), size=4)+
-  scale_color_manual(name="Functional Type", values=c("darkgreen", "chartreuse3", "green", "darkblue", "lightblue", "deepskyblue"), breaks = c("C4 Gram.", "C3 Gram.",  "Annual Gram.","Non-N-Fixing Forb", "N-Fixing Forb", "Annual Forb"))+
-  #geom_text_repel(max.overlaps = 10, size=5)+
-  facet_wrap(~treatment, labeller = labeller(treatment=collabel))+
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
-  ylab("Abundance")+
-  xlab("Year")
